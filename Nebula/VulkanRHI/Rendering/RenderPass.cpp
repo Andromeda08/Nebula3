71fe6ecf4885be11364056f5c37638eb178a84ea
Andromeda08 <vkovacsbalazs26@gmail.com>
2025-11-25 14:18:21 +0100

    Remove barriers

--- Nebula/VulkanRHI/Rendering/RenderPass.cpp
+++ Nebula/VulkanRHI/Rendering/RenderPass.cpp
@@ -6,6 +6,7 @@ namespace RHI
 {
     RenderPass::RenderPass(const RenderPassCreateInfo& createInfo)
     : mRenderArea(createInfo.renderArea)
+    , mColorAttachments(createInfo.colorAttachments)
     , mDepthAttachment(createInfo.depthAttachment)
     {
         mColorRenderingInfos = createInfo.colorAttachments
@@ -22,50 +23,9 @@ namespace RHI
 
     void RenderPass::execute(const vk::CommandBuffer& commandList, const std::function<void(const vk::CommandBuffer&)>& lambda) const
     {
-        std::vector<vk::ImageMemoryBarrier2> imageBarriers;
-
-        std::array<std::span<const Attachment>, 2> views = { std::span{mColorAttachments}, std::span<Attachment>{} };
-        if (mDepthAttachment.image)
-        {
-            views[1] = std::span{&mDepthAttachment, 1};
-        }
-
-        for (const auto& attachment : views | std::views::join)
-        {
-            auto newLayout = vk::ImageLayout::eColorAttachmentOptimal;
-            if (isDepthFormat(attachment.image->getProperties().format))
-            {
-                newLayout = vk::ImageLayout::eDepthAttachmentOptimal;
-            }
-
-            const auto barrier = vk::ImageMemoryBarrier2()
-                .setImage(attachment.image->getImage())
-                .setOldLayout(attachment.image->getState().layout)
-                .setNewLayout(newLayout)
-                .setSrcAccessMask(attachment.image->getState().accessFlags)
-                .setDstAccessMask(vk::AccessFlagBits2::eColorAttachmentWrite)
-                .setSrcStageMask(vk::PipelineStageFlagBits2::eAllCommands)
-                .setDstStageMask(vk::PipelineStageFlagBits2::eAllCommands);
-
-            imageBarriers.push_back(barrier);
-        }
-
-        const auto dependencyInfo = vk::DependencyInfo()
-            .setImageMemoryBarrierCount(imageBarriers.size())
-            .setPImageMemoryBarriers(imageBarriers.data());
-
-        commandList.pipelineBarrier2(dependencyInfo);
         commandList.beginRendering(&mRenderingInfo);
         lambda(commandList);
         commandList.endRendering();
-
-        for (auto& barrier : imageBarriers)
-        {
-            std::swap(barrier.oldLayout, barrier.newLayout);
-            std::swap(barrier.srcAccessMask, barrier.dstAccessMask);
-            std::swap(barrier.srcStageMask, barrier.dstStageMask);
-        }
-        commandList.pipelineBarrier2(dependencyInfo);
     }
 
     void RenderPass::setColorAttachment(const uint32_t index, const Attachment& attachment)
