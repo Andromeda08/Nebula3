d30bfdd7287cdcf752faa38e2ffcd0788b672621
Andromeda08 <vkovacsbalazs26@gmail.com>
2025-12-07 19:27:48 +0100

    [nst] Build at cmake configure time

---
+++ Tools/nst-cmake/nst.cmake
@@ -0,0 +1,7 @@
+find_program(CARGO cargo REQUIRED)
+
+execute_process(
+    COMMAND ${CARGO} build --release --target-dir ${CMAKE_BINARY_DIR}/nst
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/Tools/nst
+    RESULT_VARIABLE CARGO_RESULT
+)
