aaf376aaceb254350041a5ff987e6eec797892a9
Andromeda08 <vkovacsbalazs26@gmail.com>
2025-12-09 17:12:56 +0100

    ICamera

---
+++ Nebula/Scene/Camera/ICamera.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include "Scene/Types/CameraData.hpp"
+
+class ICamera
+{
+public:
+    ICamera() = default;
+    virtual ~ICamera() = default;
+
+    virtual const glm::vec3& eye()           const = 0;
+    virtual       glm::mat4  view()          const = 0;
+    virtual       glm::mat4  projection()    const = 0;
+    virtual       CameraData getCameraData() const = 0;
+
+    virtual void registerKeys(GLFWwindow* pWindow) {}
+    virtual void registerMouse(GLFWwindow* pWindow) {}
+};
---
+++ Nebula/Scene/Types/CameraData.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+struct CameraData
+{
+    glm::mat4 view;
+    glm::mat4 proj;
+    glm::mat4 viewInverse;
+    glm::mat4 projInverse;
+    glm::vec4 eye;
+    float     nearPlane;
+    float     farPlane;
+};
