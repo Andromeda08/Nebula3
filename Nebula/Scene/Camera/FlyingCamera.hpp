101dd4d000d694615d21c69b9ec07ac3ee43c45b
Andromeda08 <vkovacsbalazs26@gmail.com>
2026-01-28 14:30:47 +0100

    FlyingCamera.cpp

--- Nebula/Scene/Camera/FlyingCamera.hpp
+++ Nebula/Scene/Camera/FlyingCamera.hpp
@@ -1,155 +1,32 @@
 #pragma once
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/rotate_vector.hpp>
-#include <glm/gtx/vector_angle.hpp>
-#include <SDL3/SDL_mouse.h>
+#include <SDL3/SDL_events.h>
 #include "ICamera.hpp"
 
 class FlyingCamera final : public ICamera
 {
 public:
-    FlyingCamera(glm::ivec2 size, glm::vec3 eye, float h_fov = 75.0f, float near = 0.1f, float far = 10000.0f)
-    : ICamera(), mSize(size), mEye(eye), mNear(near), mFar(far), mFov(h_fov) {}
+    FlyingCamera(glm::ivec2 size, glm::vec3 eye, float h_fov = 75.0f, float near = 0.1f, float far = 10000.0f);
 
-    ~FlyingCamera() override = default;
+    ~FlyingCamera() override;
 
-    void onEvent(const SDL_Event& event) noexcept override
-    {
-        switch (event.type)
-        {
-            case SDL_EVENT_KEY_DOWN:
-            case SDL_EVENT_KEY_UP: {
-                handleKeyEvent(event.key);
-                break;
-            }
-            case SDL_EVENT_MOUSE_BUTTON_DOWN: {
-                if (event.button.button == SDL_BUTTON_LEFT)
-                {
-                    mMouseCaptured = true;
-                    SDL_SetWindowRelativeMouseMode(SDL_GetWindowFromID(event.button.windowID), true);
-                }
-                break;
-            }
-            case SDL_EVENT_MOUSE_BUTTON_UP: {
-                if (event.button.button == SDL_BUTTON_LEFT)
-                {
-                    mMouseCaptured = false;
-                    SDL_SetWindowRelativeMouseMode(SDL_GetWindowFromID(event.button.windowID), false);
-                }
-                break;
-            }
-            case SDL_EVENT_MOUSE_MOTION: {
-                if (mMouseCaptured)
-                {
-                    handleMouseEvent(event.motion);
-                }
-                break;
-            }
-            default:
-                break;
-        }
-    }
+    void onEvent(const SDL_Event& event) noexcept override;
 
-    void onUpdate() noexcept override
-    {
-        const glm::vec3 right = glm::normalize(glm::cross(mOrientation, mUp));
+    void onUpdate() noexcept override;
 
-        if (mKeyState.forward)
-            mEye += mSpeed * mOrientation;
-        if (mKeyState.backward)
-            mEye -= mSpeed * mOrientation;
-        if (mKeyState.left)
-            mEye -= mSpeed * right;
-        if (mKeyState.right)
-            mEye += mSpeed * right;
-        if (mKeyState.up)
-            mEye += (mSpeed / 2.0f) * mUp;
-        if (mKeyState.down)
-            mEye -= (mSpeed / 2.0f) * mUp;
-    }
+    const glm::vec3& eye() const override;
 
-    const glm::vec3& eye() const override { return mEye; }
+    glm::mat4 view() const override;
 
-    glm::mat4 view() const override
-    {
-        return glm::lookAt(mEye, mEye + mOrientation, mUp);
-    }
+    glm::mat4 projection() const override;
 
-    glm::mat4 projection() const override
-    {
-        return glm::perspective(
-            glm::radians(mFov),
-            static_cast<float>(mSize.x) / static_cast<float>(mSize.y),
-            mNear, mFar);
-    }
-
-    CameraData getCameraData() const override
-    {
-        auto v = view();
-        auto p = projection();
-        auto e = eye();
-
-        return {
-            .view = v,
-            .proj = p,
-            .viewInverse = glm::inverse(v),
-            .projInverse = glm::inverse(p),
-            .eye = { e.x, e.y, e.z, 1.0f },
-            .nearPlane = mNear,
-            .farPlane = mFar,
-        };
-    }
+    CameraData getCameraData() const override;
 
 private:
-    void handleKeyEvent(const SDL_KeyboardEvent& keyEvent) noexcept
-    {
-        const bool pressed = (keyEvent.type == SDL_EVENT_KEY_DOWN);
+    void handleKeyEvent(const SDL_KeyboardEvent& keyEvent) noexcept;
 
-        switch (keyEvent.scancode)
-        {
-            case SDL_SCANCODE_W:
-                mKeyState.forward = pressed;
-                break;
-            case SDL_SCANCODE_S:
-                mKeyState.backward = pressed;
-                break;
-            case SDL_SCANCODE_A:
-                mKeyState.left = pressed;
-                break;
-            case SDL_SCANCODE_D:
-                mKeyState.right = pressed;
-                break;
-            case SDL_SCANCODE_SPACE:
-                mKeyState.up = pressed;
-                break;
-            case SDL_SCANCODE_LCTRL:
-                mKeyState.down = pressed;
-                break;
-            default:
-                break;
-        }
-    }
-
-    void handleMouseEvent(const SDL_MouseMotionEvent& motionEvent) noexcept
-    {
-        const float rotX = mSensitivity * motionEvent.yrel / mSize.y;
-        const float rotY = mSensitivity * motionEvent.xrel / mSize.x;
-
-        const glm::vec3 newOrientation = glm::rotate(
-            mOrientation,
-            glm::radians(-rotX),
-            glm::normalize(glm::cross(mOrientation, mUp))
-        );
-
-        if (glm::abs(glm::angle(newOrientation, mUp) - glm::radians(90.0f)) <= glm::radians(85.0f))
-        {
-            mOrientation = newOrientation;
-        }
-
-        mOrientation = glm::rotate(mOrientation, glm::radians(-rotY), mUp);
-    }
+    void handleMouseEvent(const SDL_MouseMotionEvent& motionEvent) noexcept;
 
     glm::ivec2 mSize;
     glm::vec3  mEye;
