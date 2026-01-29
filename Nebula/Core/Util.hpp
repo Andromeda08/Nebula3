0c1f9220d864fb4fa5113aafd82b7cd4e0b6c64f
Andromeda08 <vkovacsbalazs26@gmail.com>
2026-01-29 17:45:38 +0100

    exitWithError and exitOnAssert utils

--- Nebula/Core/Util.hpp
+++ Nebula/Core/Util.hpp
@@ -3,6 +3,34 @@
 #include <format>
 #include <print>
 #include <string_view>
+#include <spdlog/spdlog.h>
+
+template <class... Args>
+[[noreturn]] void exitWithError(spdlog::format_string_t<Args...> fmt, Args &&...args) noexcept
+{
+    spdlog::set_level(spdlog::level::critical);
+    spdlog::critical(fmt, std::forward<Args>(args)...);
+
+    // "portable" __debugbreak() / __builtin_debugtrap()
+    #if defined(__x86_64__) || defined(_M_X64)
+    asm("int 3");
+    #else
+    // Breakpoint for arm platforms
+    asm("brk #0xF000");
+    #endif
+
+    std::exit(EXIT_FAILURE);
+}
+
+template <class... Args>
+void exitOnAssert(const bool condition, spdlog::format_string_t<Args...> fmt, Args&&... args) noexcept
+{
+    [[unlikely]] if (!condition)
+    {
+        exitWithError(fmt, std::forward<Args>(args)...);
+    }
+}
+
 
 namespace util::debug
 {
