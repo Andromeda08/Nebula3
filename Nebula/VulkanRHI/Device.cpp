954aac9881d70941eeb9fcf3e46d6b41e1a8cd3f
Andromeda08 <vkovacsbalazs26@gmail.com>
2026-02-13 15:29:20 +0100

    fix namespace

--- Nebula/VulkanRHI/Device.cpp
+++ Nebula/VulkanRHI/Device.cpp
@@ -73,7 +73,7 @@ namespace RHI
             .setTimelineSemaphore(true)
             .setHostQueryReset(true)
             .setScalarBlockLayout(true)
-            .setDrawIndirectCount(Platform::getDrawIndirectCountSupported());
+            .setDrawIndirectCount(platform::getDrawIndirectCountSupported());
     });
 
     def_VulkanExt(Core13, "VulkanCore1.3", vk::PhysicalDeviceVulkan13Features, [&]() -> void {
