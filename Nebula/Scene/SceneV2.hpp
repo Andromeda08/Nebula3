c684a10ff2df5ad91266096580d1880a47088658
Andromeda08 <vkovacsbalazs26@gmail.com>
2026-02-20 14:12:19 +0100

    remove old TLAS

--- Nebula/Scene/SceneV2.hpp
+++ Nebula/Scene/SceneV2.hpp
@@ -122,83 +122,6 @@ private:
         addObject<ExampleObject>(geoCube, 1, Transform().translate({ 5.0f, 0.0f, 0.0f }));
         addObject<ExampleObject>(geoSphere, 2, Transform().translate({ 0.0f, 0.0f, 0.0f }));
         addObject<ExampleObject>(geoCylinder, 3, Transform().translate({ -5.0f, 0.0f, 0.0f }));
-
-        // buildTLAS();
-    }
-
-    void buildTLAS() noexcept
-    {
-        // Gather instance data
-        // ===================================
-        #pragma region
-        std::vector<vk::AccelerationStructureInstanceKHR> instances;
-        for (const auto& obj : mObjects)
-        {
-            const auto blas = mGeometry->getGeometryBLAS(obj->pGeometry->getName());
-            const auto instance = vk::AccelerationStructureInstanceKHR()
-                .setFlags(vk::GeometryInstanceFlagBitsKHR::eTriangleFacingCullDisable)
-                .setMask(obj->rt_mask)
-                .setAccelerationStructureReference(blas->getAddress())
-                .setInstanceShaderBindingTableRecordOffset(0)
-                .setTransform(obj->transform.getModel3x4());
-            instances.push_back(instance);
-        }
-
-        const auto instanceSize = instances.size() * sizeof(vk::AccelerationStructureInstanceKHR);
-        mTopLevelInstances = mRHI->createBuffer({
-            instanceSize, RHI::BufferType::Storage
-        });
-        mRHI->immediate_uploadToBuffer(mTopLevelInstances.get(), instances.data(), instanceSize, 0);
-        #pragma endregion
-
-        // Prepare Top Level Build
-        // ===================================
-        #pragma region
-        const auto geometryInstancesData = vk::AccelerationStructureGeometryInstancesDataKHR()
-            .setArrayOfPointers(false)
-            .setData(mTopLevelInstances->getAddress());
-
-        const auto tlasGeometryData = vk::AccelerationStructureGeometryDataKHR()
-            .setInstances(geometryInstancesData);
-
-        const auto tlasGeometry = vk::AccelerationStructureGeometryKHR()
-            .setFlags({})
-            .setGeometry(tlasGeometryData)
-            .setGeometryType(vk::GeometryTypeKHR::eInstances)
-            .setPNext(nullptr);
-
-        auto tlasBuildGeometryInfo = vk::AccelerationStructureBuildGeometryInfoKHR()
-            .setFlags(vk::BuildAccelerationStructureFlagBitsKHR::ePreferFastBuild | vk::BuildAccelerationStructureFlagBitsKHR::eAllowUpdate)
-            .setGeometryCount(1)
-            .setPGeometries(&tlasGeometry)
-            .setMode(vk::BuildAccelerationStructureModeKHR::eBuild)
-            .setType(vk::AccelerationStructureTypeKHR::eTopLevel);
-
-        const auto tlasBuildSizesInfo = mRHI->getDevice()->getHandle().getAccelerationStructureBuildSizesKHR(
-            vk::AccelerationStructureBuildTypeKHR::eDevice, tlasBuildGeometryInfo, instances.size());
-        #pragma endregion
-
-        mTopLevelData = mRHI->createBuffer({ tlasBuildSizesInfo.accelerationStructureSize, RHI::BufferType::AccelerationStructure });
-        mTopLevelAS = RHI::AccelerationStructure::create({
-            .backingBuffer = mTopLevelData,
-            .offset = 0,
-            .size = tlasBuildSizesInfo.accelerationStructureSize,
-            .type = RHI::AccelerationStructureType::TopLevel,
-            .label = "Scene-TopLevelAS"
-        }, mRHI->getDevice());
-
-        const auto buildScratch = mRHI->createBuffer({ tlasBuildSizesInfo.buildScratchSize, RHI::BufferType::Storage });
-        tlasBuildGeometryInfo
-            .setDstAccelerationStructure(mTopLevelAS->getHandle())
-            .setScratchData(buildScratch->getAddress());
-
-        const auto tlasBuildRangeInfo = vk::AccelerationStructureBuildRangeInfoKHR().setPrimitiveCount(instances.size());
-        std::vector<const vk::AccelerationStructureBuildRangeInfoKHR*> tlasBuildRanges = {&tlasBuildRangeInfo};
-
-        mRHI->getGraphicsQueue()->immediate([&](const RHI::CommandList* pCommandList) -> void
-        {
-           pCommandList->getHandle().buildAccelerationStructuresKHR(tlasBuildRanges.size(), &tlasBuildGeometryInfo, tlasBuildRanges.data());
-        });
     }
 
     SPtr<RHI::VulkanRHI>                mRHI;
@@ -209,8 +132,4 @@ private:
     UPtr<TLASManager>                   mTLASManager;
 
     std::vector<UPtr<Object>>           mObjects;
-
-    SPtr<RHI::AccelerationStructure>    mTopLevelAS;
-    SPtr<RHI::Buffer>                   mTopLevelData;
-    SPtr<RHI::Buffer>                   mTopLevelInstances;
 };
