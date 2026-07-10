01729a2219942a448d137625627ccb486a7142a7
Andromeda08 <vkovacsbalazs26@gmail.com>
2026-07-10 18:21:22 +0200

    minimal AgX tonemapper

--- Resources/Gen2_Shaders/Tonemap.frag.glsl
+++ Resources/Gen2_Shaders/Tonemap.frag.glsl
@@ -30,12 +30,61 @@ vec3 tonemap_uncharted2(vec3 x)
     return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
 }
 
+vec3 agxDefaultContrastApprox(vec3 x) {
+    vec3 x2 = x * x;
+    vec3 x4 = x2 * x2;
+
+    return + 15.5     * x4 * x2
+           - 40.14    * x4 * x
+           + 31.96    * x4
+           - 6.868    * x2 * x
+           + 0.4298   * x2
+           + 0.1191   * x
+           - 0.00232;
+}
+
+vec3 agx(vec3 val) {
+    const mat3 agx_mat = mat3(
+        0.842479062253094,  0.0423282422610123, 0.0423756549057051,
+        0.0784335999999992, 0.878468636469772,  0.0784336,
+        0.0792237451477643, 0.0791661274605434, 0.879142973793104
+    );
+
+    const float min_ev = -12.47393f;
+    const float max_ev = 4.026069f;
+
+    val = agx_mat * val;
+
+    val = clamp(log2(val), min_ev, max_ev);
+    val = (val - min_ev) / (max_ev - min_ev);
+
+    val = agxDefaultContrastApprox(val);
+
+    return val;
+}
+
+vec3 agxEotf(vec3 val) {
+    const mat3 agx_mat_inv = mat3(
+         1.19687900512017,   -0.0528968517574562, -0.0529716355144438,
+        -0.0980208811401368,  1.15190312990417,   -0.0980434501171241,
+        -0.0990297440797205, -0.0989611768448433,  1.15107367264116
+    );
+
+    val = agx_mat_inv * val;
+    val = pow(val, vec3(2.2));
+
+    return val;
+}
+
 void main()
 {
     vec3 color = texture(uColor, inUV).rgb;
 
-    vec3 mapped = tonemap_uncharted2(color * exposure);
-    vec3 whiteScale = 1.0 / tonemap_uncharted2(vec3(11.2));
+    vec3 value = agx(color);
+    value = agxEotf(value);
+    outColor = vec4(value, 1.0);
 
-    outColor = vec4(mapped * whiteScale, 1.0);
+    // vec3 mapped = tonemap_uncharted2(color * exposure);
+    // vec3 whiteScale = 1.0 / tonemap_uncharted2(vec3(11.2));
+    // outColor = vec4(mapped * whiteScale, 1.0);
 }
